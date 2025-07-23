ad7d898c14b84496626ac27214f7d1477f223b74
wilffren <wilffren14@gmail.com>
2025-07-23 16:47:20 -0500

    Feat: THe login js was created

--- Training-4/src/js/login.js
+++ Training-4/src/js/login.js
@@ -1,6 +1,31 @@
-// Send other view
-document.getElementById('loginForm').addEventListener('submit', function (event) {
-  event.preventDefault(); // Previene que se recargue la página
+//Validate the login form
+
+const loginForm = document.getElementById('loginForm');
+
+loginForm.addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    const username = loginForm.username.value
+    const password = loginForm.password.value;
+
+    login(username, password)
+}) 
+
+async function login(username, password) {
+  let response = await fetch(`http://localhost:3000/user?username=${username}`, )
+  let data = await response.json();
   
-  window.location.href = './views/movements.html';
-});
+  if (data.length === 0) {
+        alert("credenciales incorrectas, revisa el correo o la contraseña")
+    } else {
+        const userFound = data[0]
+
+        if (userFound.password === password) {
+            localStorage.setItem('user', JSON.stringify(userFound));
+            window.location.href = 'index.html';
+        } else {
+            alert("credenciales incorrectas, revisa el correo o la contraseña");
+        }
+      }
+      
+}
