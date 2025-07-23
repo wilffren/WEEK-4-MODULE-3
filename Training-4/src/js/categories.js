c83697ed6da7ac2394461b4425331fbaabcece65
wilffren <wilffren14@gmail.com>
2025-07-23 18:48:22 -0500

    Feat: The method Post was cretaed for categories

--- Training-4/src/js/categories.js
+++ Training-4/src/js/categories.js
@@ -5,3 +5,33 @@ document.getElementById("return").addEventListener("click", () => {
 
 // categories endpoint 
 const endPoint = "http://localhost:3000/categories";
+
+//button for save categories
+const btnCategorie = document.getElementById("btn-categorie");
+const inputText = document.getElementById("categorie");
+
+
+btnCategorie.addEventListener("click", (event) => {
+    event.preventDefault(); // Prevent form submission
+
+    const newCategory = {
+        name: inputText.value
+    }
+    saveCategorie(newCategory);
+});
+
+// Fetch categories from the server
+
+async function saveCategorie(newCategory) {
+    let response = await fetch(endPoint, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newCategory)
+    });
+
+    if(response.ok){
+        alert("categorie created")
+    }
+}
