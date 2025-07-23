bdfe70abf26b13b0a89983739d6e72642400b792
wilffren <wilffren14@gmail.com>
2025-07-23 18:31:18 -0500

    Feat: save the endpoint

--- Training-4/src/js/categories.js
+++ Training-4/src/js/categories.js
@@ -3,3 +3,5 @@ document.getElementById("return").addEventListener("click", () => {
     window.location.href = "dashboard.html";
 })
 
+// categories endpoint 
+const endPoint = "http://localhost:3000/categories";
