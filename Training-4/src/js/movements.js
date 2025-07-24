80a00846e71510c84d9a8daddf38e5f128481d3c
wilffren <wilffren14@gmail.com>
2025-07-24 15:29:46 -0500

    Feat: Reestructure the logic

--- Training-4/src/js/movements.js
+++ Training-4/src/js/movements.js
@@ -3,6 +3,9 @@ document.getElementById("return").addEventListener("click", () => {
     window.location.href = "dashboard.html";
 })
 
+
+//endpoint for movements
+const endPointMovements = "http://localhost:3000/movements";
 // function to save movements
 
 const $formMovements = document.getElementById("form-movements");
