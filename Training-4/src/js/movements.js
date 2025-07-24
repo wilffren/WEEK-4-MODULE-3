8c5b250d8fa9ba83f7f045e921945a58805bdc20
wilffren <wilffren14@gmail.com>
2025-07-24 14:35:41 -0500

    Feat: Reestructure of movements

--- Training-4/src/js/movements.js
+++ Training-4/src/js/movements.js
@@ -3,3 +3,10 @@ document.getElementById("return").addEventListener("click", () => {
     window.location.href = "dashboard.html";
 })
 
+// function to save movements
+
+const $formMovements = document.getElementById("form-movements");
+$formMovements.addEventListener("submit",  (event) => {
+    event.preventDefault();
+         })                                       
+    
