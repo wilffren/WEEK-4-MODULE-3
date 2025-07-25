fce56ae05b035859630b91f6d3d0308d45e9a776
wilffren <wilffren14@gmail.com>
2025-07-25 17:26:01 -0500

    Feat: Document code was created

--- Training-4/src/js/guardian.js
+++ Training-4/src/js/guardian.js
@@ -1,3 +1,6 @@
+// guardian.js
+// This script checks if a user is logged in by verifying the presence of 'currentUser'
+// in localStorage. If not found, it redirects to the index page.
 function checkSession() {
     let checkUser = localStorage.getItem('currentUser');
 
